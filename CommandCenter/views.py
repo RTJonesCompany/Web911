dd9216f12edc45cba0d54f1e36712af945cd1bab
Ryan Jones <Ryan@RTJonesCo.com>
2016-07-15 03:33:11 +0000

    inital routing for the command center module cmdctr

--- CommandCenter/views.py
+++ CommandCenter/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#for inital routing testing only
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Welcome to the command center module for the web911 application.")
--- web911/settings.py
+++ web911/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'DispatchConsole',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
