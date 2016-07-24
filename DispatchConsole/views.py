0c8bfde626b284548a4281a773697ba4b7e5fe38
Ryan Jones <ryan@rtjonesco.com>
2016-07-23 22:06:24 -0400

    nightly commit

--- DispatchConsole/views.py
+++ DispatchConsole/views.py
@@ -6,5 +6,10 @@ from django.http import HttpResponse
 #this index loads a example template with snippets {ryan}
 def index(request):
     all_incidents = incidentdata.get_incident_list()
-    
+
     return render(request,'main/current.html', {"incidents": all_incidents})
+
+def show(request,iid):
+    one_incident = incidentdata.get_incident(iid)
+
+    return render(request,'main/incident-info.html', {"one_incident": one_incident})
